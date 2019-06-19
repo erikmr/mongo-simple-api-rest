9a1ae698fb0d41e104a90f55c618070860f37db5
Erik Martinez <emartinez@ensamble.cc>
2019-06-19 08:31:00 -0500

    add endpoints

--- index.js
+++ index.js
@@ -3,11 +3,38 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended :false}))
 app.use(bodyParser.json())
+
+app.get('/api/contracts',(req, res) =>{
+    res.status(200).send({contracts:[]})
+})
+
+app.get('/api/contracts/:contractId',(req, res) =>{
+    
+})
+
+app.post('/api/contract',(req, res) =>{
+    console.log(req.body)
+    res.status(200).send({message:'El contrato se ha registrado'})
+    
+})
+
+app.put('/api/contracts/:contractId',(req, res) =>{
+    
+})
+
+app.delete('/api/contracts/:contractId',(req, res) =>{
+    
+})
+
+
+
+
 app.listen(port, ()=>{
     console.log(`Start Simple API Rest App in port: ${port}`)
 })
